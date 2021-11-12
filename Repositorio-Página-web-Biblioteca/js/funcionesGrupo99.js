465e7cc1b6ed8f19a311261cf885d2ac57270b68
amibiol <92000104+amibiol@users.noreply.github.com>
2021-11-12 14:50:43 -0300

    Update funcionesGrupo99.js

--- Repositorio-Página-web-Biblioteca/js/funcionesGrupo99.js
+++ Repositorio-Página-web-Biblioteca/js/funcionesGrupo99.js
@@ -2,3 +2,15 @@
 function myFunction() {
     alert("HAS INGRESADO A LA BIBLIOTECA");
 }
+
+function cambiarImagen() {
+    const Imagen = document.getElementById("foto")
+    Imagen.src = "images/young.jpg"
+
+}
+
+function mostrarOriginal() {
+    const imagen = document.getElementById("foto")
+    imagen.src = "images/libros.jpg"
+
+}
